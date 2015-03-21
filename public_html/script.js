f85367ff94ee1d79dac03236bea7eec131796739
Stevo5o <stevo.joc@gmail.com>
2015-03-21 15:40:13 +0000

    4.1 sum of arrange and step

--- public_html/script.js
+++ public_html/script.js
@@ -13,24 +13,33 @@
 (function() {
 
 	// exercises
-	// 4.1 sum of arrange
-	function range(start, end) {
-
+	// 4.1 sum of arrange and step
+	function range(start, end, step) {
+		if (step == null) {
+			step = 1;
+		}
 		var arr = [];
 
-		for (var i = start; i <= end; i++) {
-			arr.push(i);
+		if(step > 0) {
+			for(vari = start; i <= end; i += step)
+			{
+				arr.push(i);
+			}
+		} else {
+			for(var i = start; i >= end; i += step)
+			{
+				arr.push(i);
+			}			
 		}
 		return arr;
 	};
 
 	function sum(arr) {
-		var total = 0;		
-
+		var total = 0;
 		for (var i = 0; i < arr.length; i++) {
 			total += arr[i];
 		}
 		return total;
-	};	
+	};
 	console.log(sum(range(1, 10)));
 }());
